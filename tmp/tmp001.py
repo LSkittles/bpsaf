3c2fdc0c16e91f0a0329b2021d4b0f20d47c151c
LSkittles <lskittles@foxmail.com>
2024-06-15 18:04:25 +0800

    as

--- tmp/tmp001.py
+++ tmp/tmp001.py
@@ -1,10 +1,18 @@
 # 仅供测试使用
 import pyvex
 import archinfo
+from TargetFacts import TargetFacts
+from IRSB2Facts import IRSB2Facts
 # 翻译ADD EBX, 0x4; ADD EBX, 0x4
 # 同一个基本块内，EBX只会被赋值一次。
 irsbtmp = pyvex.lift(data=b"\x81\xc3\x04\x00\x00\x00\x81\xc3\x04\x00\x00\x00", addr=0x400400, arch=archinfo.ArchX86())
 irsbtmp.pp()
+
+results = TargetFacts()
+eid_iter = iter(range(1, 100_000_000_000))
+results.arch = archinfo.ArchX86()
+IRSB2Facts(irsbtmp, results, eid_iter=eid_iter)
+pass
 
 #  LEA ECX, [ESP+0x4]
 # irsbtmp = pyvex.lift(b"\x8d\x4c\x24\x04", 0x400400, archinfo.ArchX86())
